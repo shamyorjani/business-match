d6e877063b262854191071aee55aa3bba87515e2
shamyorjani <shamyorjani@gmail.com>
2025-03-12 17:25:07 +0500

    puhs

--- resources/js/pages/Interest.jsx
+++ resources/js/pages/Interest.jsx
@@ -248,9 +248,3 @@ const CompanyInfo = () => {
 };
 
 export default CompanyInfo;
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/shamyorjani/business-match.git
-git push -u origin main
