325dfe0843231255aac86d965d7d01647ae3eeeb
hassanjan1841 <hijan29714@gmail.com>
2025-03-25 14:40:53 +0500

    solved the issue of the composer install error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,33 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use App\Exceptions\Handler;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-        apiPrefix: '/api',
+        web: __DIR__."/../routes/web.php",
+        api: __DIR__."/../routes/api.php",
+        commands: __DIR__."/../routes/console.php",
+        health: "/up",
+        apiPrefix: "/api",
     )
-    ->withMiddleware(function ($middleware) {
-        // Define your middleware here
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->dontReport([
+            //
+        ]);
+        
+        $exceptions->reportable(function (\Throwable $e) {
+            //
+        });
+
+        $exceptions->renderable(function (\Throwable $e) {
+            $handler = app()->make(Handler::class);
+            return $handler->render(request(), $e);
+        });
     })
     ->create();
